d4917cf28984a87409f1ad61e19969bd38a9c18e
sumin-tail <sumin4117@gmail.com>
2021-04-05 21:37:53 +0900

    유니티 수정
    맵생성 타일맵

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0f;
+    public Rigidbody2D rb;
+    public float jump;
+
+    void Update()
+    {
+        MoveMent();
+    }
+    void MoveMent()
+    {
+        float HorizontalMove = Input.GetAxis("Horizontal");
+        float FaceDircetion = Input.GetAxisRaw("Horizontal");
+
+        if (HorizontalMove != 0)
+        {
+            rb.velocity = new Vector2(HorizontalMove * speed, rb.velocity.y);
+        }
+        if (FaceDircetion != 0)
+        {
+            transform.localScale = new Vector3(FaceDircetion, 1, 1);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+
+    }
+}
