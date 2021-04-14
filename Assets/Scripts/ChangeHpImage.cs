75cba71d485237d7304cd1f44f0cc58440415d95
sumin-tail <sumin4117@gmail.com>
2021-04-14 20:13:54 +0900

    맵과 hp UI

---
+++ Assets/Scripts/ChangeHpImage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeHpImage : MonoBehaviour
+{
+    //이미지를 담을 컴포넌트 image
+    private Image image;
+
+    //인스펙터뷰에서 채우기 위해 SerializeField 를 적용시킴
+    [SerializeField]
+    private Slider sliderHp; //플레이어 Hp 슬라이더 
+    [SerializeField]
+    private Sprite maxHp; //풀피일때의 이미지
+    [SerializeField]
+    private Sprite Hp; //풀피가 아닐 때의 이미지
+
+
+    void Start()
+    {
+        //게임오브젝트에있는 Image 컴포넌트 가져옴
+        image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (sliderHp.value == 100)
+        {
+            image.sprite = maxHp;
+        }
+
+        if (sliderHp.value < 100)
+        {
+            image.sprite = Hp;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,31 +5,74 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //스피드랑 점프 스케일 적용하기
-    public float speed = 3f;
-    public Rigidbody2D rb;
-    public float jump = 10f;
+    public float speed = 4f;
+    public float jump = 15f;
+    public GameManager gameManager;
+    //리지드 바디 설정
+    Rigidbody2D rb;
+    //스프라이트 렌더 설정
+    SpriteRenderer spriteRenderer;
+
+
+    void Awake()
+    {
+        //해당되는 게임 컴포넌트의 리지드 바디를 가져옴
+        rb = GetComponent<Rigidbody2D>();
+        //렌더 가져옴
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
         MoveMent();
     }
+
     void MoveMent()
     {
+        //좌우이동. GetAxis는 -1.0f 부터 1.0f 까지의 범위의 값을 반환 > 아래에서 부드러운 이동용으로 사용
+        //0에서 좌이동일경우 -1.0f로 우이동일 경우 1.0f로 올라감 
         float HorizontalMove = Input.GetAxis("Horizontal");
+        //플레이어 이미지 방향 GetAxisRaw는 -1, 0, 1 세 가지 값 중 하나가 반환. 즉각적인 반응에 사용
         float FaceDircetion = Input.GetAxisRaw("Horizontal");
 
         if (HorizontalMove != 0)
         {
+            //천천히 플레이어 속도를 4f또는 -4f 까지 올림
             rb.velocity = new Vector2(HorizontalMove * speed, rb.velocity.y);
         }
+
         if (FaceDircetion != 0)
         {
             transform.localScale = new Vector3(FaceDircetion, 1, 1);
         }
+
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jump);
         }
+    }
 
+    //에너미 충돌 체크
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            OnDamage();
+        }
+    }
+
+    //무적상태
+    void OnDamage()
+    {
+        gameManager.sliderHp.value -= 10f;    
+        gameObject.layer = 11;
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        Invoke("OffDamage", 1);
+    }
+    //무적상태 해제
+    void OffDamage()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
     }
 }
